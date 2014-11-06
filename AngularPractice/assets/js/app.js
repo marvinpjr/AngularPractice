a77bfea6d3ccbf6418734f3331fecd382b7bfa94
PJU8560 <marvin.palmer@healthtrustpg.com>
2014-11-06 13:36:11 -0600

    added directives to loop through an array of products

---
+++ AngularPractice/assets/js/app.js
@@ -0,0 +1,28 @@
+﻿// wrap in a closure
+(function() {
+    // define module with no dependencies
+    var app = angular.module('store', []);
+
+    // define a controller
+    app.controller('StoreController', function() {
+        this.products = gems;
+    });
+
+    var dodeca = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: 'lovely ten sided gem',
+        canPurchase: true,
+        soldOut: false
+    }
+
+    var pent = {
+        name: 'Pentagonal Gem',
+        price: 5.95,
+        description: 'fancy five sided gem',
+        canPurchase: false,
+        soldOut: false
+    }
+    var gems = [dodeca, pent];
+
+})();
