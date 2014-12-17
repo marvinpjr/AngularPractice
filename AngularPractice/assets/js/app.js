f676a5fd51b7ccc2addf26515528d87bf00a3f84
PJU8560 <marvin.palmer@healthtrustpg.com>
2014-12-17 14:44:07 -0600

    Doesn't work.  Just making a checkpoint.  Call to angular http service isn't working.

--- AngularPractice/assets/js/app.js
+++ AngularPractice/assets/js/app.js
@@ -4,10 +4,22 @@
     var app = angular.module('store', ['store-products']);
 
     // define controllers
-    app.controller('StoreController', function () {
-        this.products = gems;
-    });
+    app.controller('StoreController', ['$http', '$location', function($http, $location) {
+        var store = this;
+        store.products = [];
 
+        var path =
+            //$location.protocol() + '://' + $location.host() + ":" + $location.port() +
+            '/assets/js/products.json'; // why the 404?
+        $http.get(path)
+            .success(function (data) {
+                store.products = data; // gems
+            })
+            .error(function(error) {
+                console.log(error); // how do we use this?
+            });
+    }]);
+    
     app.controller('ReviewController', function() {
         this.review = {};
 
---
+++ AngularPractice/assets/js/products.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    var app = angular.module('store-products', []);
+
+    app.directive('productTitle', function () {
+        return {
+            restrict: 'E', // stands for element, as in new html element; A would indicate an attribute on an element
+            templateUrl: 'templates/product-title.html' // url of template
+        };
+    });
+
+    app.directive('productPanels', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/product-panels.html',
+            controller: function () { PanelController(this); },
+            controllerAs: "panel"
+        };
+    });
+
+    function PanelController(directive) {
+        directive.tab = 1;
+
+        directive.selectTab = function (setTab) {
+            this.tab = setTab;
+        }
+
+        directive.isSelected = function (checkTab) {
+            return directive.tab === checkTab;
+        }
+    }
+})();
