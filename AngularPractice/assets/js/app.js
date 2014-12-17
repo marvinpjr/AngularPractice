fca312466401e0f8d214654e621137896df5482a
PJU8560 <marvin.palmer@healthtrustpg.com>
2014-12-17 10:49:38 -0600

    wrapped direction definition of controller in closure and call to function of same name

--- AngularPractice/assets/js/app.js
+++ AngularPractice/assets/js/app.js
@@ -1,7 +1,7 @@
 ﻿// wrap in a closure
 (function () {
     // define module with no dependencies
-    var app = angular.module('store', []);
+    var app = angular.module('store', ['store-products']);
 
     // define controllers
     app.controller('StoreController', function () {
@@ -14,44 +14,6 @@
         this.addReview = function(product) {
             product.reviews.push(this.review);
             this.review = {};
-        };
-    });
-
-    //app.controller('PanelController', function () {
-    //    this.tab = 1;
-
-    //    this.selectTab = function (setTab) {
-    //        this.tab = setTab;
-    //    }
-
-    //    this.isSelected = function (checkTab) {
-    //        return this.tab === checkTab;
-    //    }
-    //});
-
-    app.directive('productTitle', function() {
-        return {
-            restrict: 'E', // stands for element, as in new html element; A would indicate an attribute on an element
-            templateUrl: 'templates/product-title.html' // url of template
-        };
-    });
-
-    app.directive('productPanels', function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'templates/product-panels.html',
-            controller: function() {
-                this.tab = 1;
-
-                this.selectTab = function (setTab) {
-                    this.tab = setTab;
-                }
-
-                this.isSelected = function (checkTab) {
-                    return this.tab === checkTab;
-                }
-            },
-            controllerAs: "panel"
         };
     });
 })();
